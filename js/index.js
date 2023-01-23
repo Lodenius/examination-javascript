70448f60dda42f85a3c7fc440affe693bda613b1
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-01-23 14:58:15 +0100

    test

--- js/index.js
+++ js/index.js
@@ -7,3 +7,21 @@
  document.querySelector('figure').classList.add('legs')
 
  */
+
+
+
+let gameOver = document.getElementsByClassName('game-over');
+let hangman = document.getElementsByClassName('man');
+let noMatch = document.getElementsByClassName('nomatch');
+let word = document.getElementsByClassName('word');
+let randomWord = ['frontend', 'candle', 'maja', 'computer'];
+
+
+
+ 
+ 
+ const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+ 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+ 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+ 
+
