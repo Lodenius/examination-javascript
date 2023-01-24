bad4428b2c0f86c7827f76cf2e955a2b30a9b9b6
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2023-01-24 13:17:46 +0100

    n

--- js/index.js
+++ js/index.js
@@ -1,75 +1,229 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
+//  document.querySelector('figure').classList.add('scaffold')
+//  document.querySelector('figure').classList.add('head')
+//  document.querySelector('figure').classList.add('body')
+//  document.querySelector('figure').classList.add('arms')
+//  document.querySelector('figure').classList.add('legs')
 
- */
+var programming_languages = [
+	"python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"
+]
 
+let answer = '';
+let maxWrong = 5;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
 
+function randomWord() {
+  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
 
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
 
-// Get elements
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
 
-let gameOver = document.getElementsByClassName('game-over');
-let hangman = document.getElementsByClassName('man');
-let noMatch = document.getElementsByClassName('nomatch');
-let word = document.getElementsByClassName('word');
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
 
-
-
-
-let randomWord = ['frontend', 'candle', 'maja', 'computer'];
-let programmingWords = ['frontend', 'developer', 'python', 'computer', 'jhgksdhjfglkh'];
- function randomWord() {
-    answer = programmingWords[Math.floor(Math.random() * programming.length)];
- }
- randomWord();
- 
- const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
- 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
- 't', 'u', 'v', 'w', 'x', 'y', 'z'];
- 
-
-
- var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
+  if(answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
   }
+}
+
+function updateHangmanPicture() {
+  // document.getElementById('hangmanPic').src = './images' + mistakes + '.jpg';
+}
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You won';
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'the answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You lost';
+  }
+}
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
 
 
-// Create the alfabetet
+function reset() {
+  mistakes - 0;
+  guessed = []
+  document.getElementsById('scaffold');
 
+randomWord();
+guessedWord();
+updateMistakes();
+generateButtons();
+}
 
- for (const i = 0; i < alphabet.length; i++);
-
-
-
-
-// Hangman
-
-
-
-// Play
-
-
+document.getElementById('maxWrong').innerHTML = maxWrong;
 
 
 
-// Reset - Game over
+randomWord();
+generateButtons();
+guessedWord();
 
 
 
-// Reset - Win
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  För att toggla SVG:en
+//  document.querySelector('figure').classList.add('scaffold')
+//  document.querySelector('figure').classList.add('head')
+//  document.querySelector('figure').classList.add('body')
+//  document.querySelector('figure').classList.add('arms')
+//  document.querySelector('figure').classList.add('legs')
+
+//  */
+
+// // const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+// // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+// // 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+// let answer = '';
+// // let maxWrong = 5;
+// let guessed = [];
+
+
+
+
+// // Get elements
+
+// let gameOver = document.getElementsByClassName('game-over');
+// let hangman = document.getElementsByClassName('man');
+// let noMatch = document.getElementsByClassName('nomatch');
+// let word = document.getElementsByClassName('word');
+
+
+
+
+
+ 
+
+
+
+// // Create the alfabetet
+
+
+// for (const i = 0; i < alphabet.length; i++);
+
+
+
+
+// // Word
+
+// let programmingWords = ['frontend', 'developer', 'python', 'computer', 'jhgksdhjfglkh'];
+//  function randomWord() {
+//     answer = programmingWords[Math.floor(Math.random() * programming.length)];
+//  }
+//  randomWord();
+ 
+
+
+// // Hangman
+
+
+
+
+
+
+// // Play
+
+
+
+
+
+// // Game over
+
+
+
+
+
+// // Win
+
+
+
+
+
+// // Reset - Game over
+
+
+
+
+// // Reset - Win
+
+
+
